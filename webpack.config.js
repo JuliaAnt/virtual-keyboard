9d000117bdb605a5d64954c7545fbd4ceedd51fb
Julia Antoshkina <julia.antoshkina@gmail.com>
2022-05-09 21:41:38 +0200

    feat: add publish

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,15 @@
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
 module.exports = {
   entry: './js/script.js',
+  output: {
+    filename: 'js/main.js',
+  },
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [{ from: 'index.html' }],
+      patterns: [{ from: 'assets', to: 'assets' }],
+      patterns: [{ from: 'css', to: 'css' }],
+    }),
+  ],
 };
